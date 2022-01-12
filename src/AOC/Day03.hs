8e698e8073b323fbb8fab7911198e0a8b2a804aa
Jason Davidson <jason.davidson@bitnomial.com>
2022-01-12 14:19:46 -0800

    day3 solution

--- src/AOC/Day03.hs
+++ src/AOC/Day03.hs
@@ -1,11 +1,78 @@
+{-# LANGUAGE LambdaCase #-}
+
 module AOC.Day03 (day03) where
 
 import AOC.Types
+import Control.Applicative (liftA2)
+import Data.List (foldl', sort, transpose, uncons)
 
-day03 :: Solution [Int] Int Int
+day03 :: Solution [[Bit]] Int Int
 day03 =
     Solution
-        { parse = const Nothing
-        , part1 = const Nothing
-        , part2 = const Nothing
+        { parse = traverse parseLine . lines
+        , part1 = compute gammaRate epsilonRate
+        , part2 = compute oxygenGeneratorRating co2ScrubberRating
         }
+
+data Bit = Z | O
+    deriving (Show, Eq)
+
+toInt :: [Bit] -> Int
+toInt = foldl' (\n b -> n * 2 + toDigit b) 0
+  where
+    toDigit = \case
+        Z -> 0
+        O -> 1
+
+parseLine :: String -> Maybe [Bit]
+parseLine = traverse toBit
+  where
+    toBit = \case
+        '0' -> Just Z
+        '1' -> Just O
+        _ -> Nothing
+
+data BitCounts = BitCounts Int Int
+
+bitCounts :: [Bit] -> BitCounts
+bitCounts bs = BitCounts (bitCount Z) (bitCount O)
+  where
+    bitCount b = length $ filter (b ==) bs
+
+processReport :: (BitCounts -> Bit) -> (Bit -> Bit -> Bool) -> [[Bit]] -> Maybe Int
+processReport computeBit numFilter = go []
+  where
+    go :: [Bit] -> [[Bit]] -> Maybe Int
+    go bs bss
+        | length bss <= 1 = Just . toInt $ reverse bs <> concat bss
+        | otherwise = do
+            parts <- traverse uncons bss
+            let b = computeBit . bitCounts $ map fst parts
+                bss' = filter (not . null) . map snd $ filter (numFilter b . fst) parts
+            go (b:bs) bss'
+
+noFilter :: Bit -> Bit -> Bool
+noFilter _ _ = True
+
+gammaRate :: [[Bit]] -> Maybe Int
+gammaRate = processReport computeBit noFilter
+  where
+    computeBit (BitCounts z o) = if z >= o then Z else O
+
+epsilonRate :: [[Bit]] -> Maybe Int
+epsilonRate = processReport computeBit noFilter
+  where
+    computeBit (BitCounts z o) = if z <= o then Z else O
+
+oxygenGeneratorRating :: [[Bit]] -> Maybe Int
+oxygenGeneratorRating = processReport computeBit (==)
+  where
+    computeBit (BitCounts z o) = if z > o then Z else O
+
+co2ScrubberRating :: [[Bit]] -> Maybe Int
+co2ScrubberRating = processReport computeBit (==)
+  where
+    computeBit (BitCounts z o) = if z <= o then Z else O
+
+compute :: ([[Bit]] -> Maybe Int) -> ([[Bit]] -> Maybe Int) -> [[Bit]] -> Maybe Int
+compute f g bss = liftA2 (*) (f bss) (g bss)
--- test/AOC/Day03Spec.hs
+++ test/AOC/Day03Spec.hs
@@ -1,10 +1,37 @@
 module AOC.Day03Spec (spec) where
 
 import AOC (day03, parse, part1, part2)
+import Control.Monad ((<=<))
 import Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "day n ..." $ do
-        it "no-op" $ do
-            () `shouldBe` ()
+    describe "day03 part1" $ do
+        it "power consumption" $ do
+            go1 input `shouldBe` Just 198
+    describe "day03 part2" $ do
+        it "life support rating" $ do
+            go2 input `shouldBe` Just 230
+
+go1 :: String -> Maybe Int
+go1 = part1 day03 <=< parse day03
+
+go2 :: String -> Maybe Int
+go2 = part2 day03 <=< parse day03
+
+input :: String
+input =
+    unlines
+        [ "00100"
+        , "11110"
+        , "10110"
+        , "10111"
+        , "10101"
+        , "01111"
+        , "00111"
+        , "11100"
+        , "10000"
+        , "11001"
+        , "00010"
+        , "01010"
+        ]
